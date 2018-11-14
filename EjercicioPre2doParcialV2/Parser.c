3a5e4a9a88a0dbedf320ddd3fdf3a7935cea9d18
alumno <alumno@LAB00PC00>
2018-11-13 22:10:35 -0300

    guardar

---
+++ EjercicioPre2doParcialV2/Parser.c
@@ -0,0 +1,15 @@
+#include "LinkedList.h"
+
+int parser_parseEmpleados(char* fileName, LinkedList* listaEmpleados)
+{
+
+    FILE* pFile;
+    char bufferId[1024];
+    char bufferNombre[1024];
+    char bufferHoras[1024];
+
+
+
+    return 1; // OK
+}
+
